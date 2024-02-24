c5529ca88d2b670e8cc3c7060b65ec14887fd08e
UASTRONGMAN <shkolniy.maks@ukr.net>
2024-02-24 21:28:45 +0200

    Lesson 2

---
+++ Lesson 2/arr_obj.js
@@ -0,0 +1,116 @@
+// -------------------FIRST TASK-------------------
+// let arr = [1, 2, -3, 1.5, true, false, 'owu', "next_level", true, false];
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+// console.log(arr[6])
+// console.log(arr[7])
+// console.log(arr[8])
+// console.log(arr[9])
+
+
+// -------------------SECOND TASK-------------------
+// let book_1 = {
+//     title: "Pretty Things",
+//     pageCount: 576,
+//     genre: "Modern_prose"
+// };
+// let book_2 = {
+//     title: "When a Man's Single",
+//     pageCount: 248,
+//     genre: "Classical prose"
+// };
+// let book_3 = {
+//     title: "Animal Farm",
+//     pageCount: 120,
+//     genre: "fantasy"
+// };
+
+
+// -------------------THIRD TASK-------------------
+// let book_4 = {
+//     title: "Pretty Things",
+//     pageCount: 576,
+//     genre: "Modern_prose",
+//     authors:['Janell Brown', 54]
+// };
+// let book_5 = {
+//     title: "When a Man's Single",
+//     pageCount: 248,
+//     genre: "Classical prose",
+//     authors:['James Matthew Barrie', '77 (died)']
+// };
+// let book_6 = {
+//     title: "Animal Farm",
+//     pageCount: 120,
+//     genre: "fantasy",
+//     authors:['Janell Brown', '47 (died)']
+// };
+
+
+// -------------------FOURTH TASK-------------------
+let users_arr = [
+    {
+        name: "Maks",
+        username: "Maks1415",
+        password: "qwerty"
+    },
+    {
+        name: "Alex",
+        username: "Alex_boss",
+        password: "asdfwegvew"
+    },
+    {
+        name: "Den",
+        username: "Den_king",
+        password: "sadcerb"
+    },
+    {
+        name: "Vitya",
+        username: "Victory",
+        password: "dfbuym"
+    },
+    {
+        name: "Vasya",
+        username: "Winchester",
+        password: "rthnbtyh"
+    },
+    {
+        name: "Anton",
+        username: "Antoxa",
+        password: "sdvrthnuy7h123"
+    },
+    {
+        name: "Olya",
+        username: "Olga_nails",
+        password: "qergh56tgf2"
+    },
+    {
+        name: "Semen",
+        username: "Saymon_says",
+        password: "ergbt5h3r4"
+    },
+    {
+        name: "Dima",
+        username: "Dark_Dima",
+        password: "rth67j45tg"
+    },
+    {
+        name: "Tanya",
+        username: "Tankistka",
+        password: "erbh6jy754tg"
+    }
+];
+console.log(users_arr[0].password)
+console.log(users_arr[1].password)
+console.log(users_arr[2].password)
+console.log(users_arr[3].password)
+console.log(users_arr[4].password)
+console.log(users_arr[5].password)
+console.log(users_arr[6].password)
+console.log(users_arr[7].password)
+console.log(users_arr[8].password)
+console.log(users_arr[9].password)
---
+++ Lesson 2/if.js
@@ -0,0 +1,106 @@
+// -------------------FIRST TASK-------------------
+// let x = -3;
+// if (x === 0){
+//     console.log('False')
+// }else{
+//     console.log('True')
+// }
+
+
+// -------------------SECOND TASK-------------------
+// let time = 48;
+// if (time >= 0 && time <= 15){
+//     console.log('Перша чверть години')
+// }else if (time > 15 && time <= 30){
+//     console.log('Друга чверть години')
+// }else if (time > 30 && time <= 45){
+//     console.log('Третя чверть години')
+// }else if (time > 45 && time <= 59){
+//     console.log('Четверта чверть години')
+// }
+
+
+// -------------------THIRD TASK-------------------
+// let day = 10;
+// if (day >= 0 && day <= 10){
+//     console.log('Перша декада місяця')
+// }else if (day > 10 && day <= 20){
+//     console.log('Друга декада місяця')
+// }else if (day > 20 && day <= 31){
+//     console.log('Третя декада місяця')
+// }
+
+
+// -------------------FOURTH TASK-------------------
+// let day = +prompt('Enter the sequence number of the day of the week');
+// switch (day) {
+//     case 1:
+//         console.log('Monday')
+//         break
+//     case 2:
+//         console.log('Tuesday')
+//         break
+//     case 3:
+//         console.log('Wednesday')
+//         break
+//     case 4:
+//         console.log('Thursday')
+//         break
+//     case 5:
+//         console.log('Friday')
+//         break
+//     case 6:
+//         console.log('Saturday')
+//         break
+//     case 7:
+//         console.log('Sunday')
+//         break
+// }
+
+
+// -------------------FIFTH TASK-------------------
+// let num_1 = +prompt('Enter first number');
+// let num_2 = +prompt('Enter second number');
+// if (num_1 > num_2) {
+//     console.log(num_1)
+// }else if (num_1 < num_2) {
+//     console.log(num_2)
+// }else {
+//     console.log('Try again')
+// }
+
+
+// -------------------SIXTH TASK-------------------
+// let x;
+// x = x || 'default';
+// console.log(x)
+
+
+// -------------------SEVENTH TASK-------------------
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log('Супер')
+}
+
