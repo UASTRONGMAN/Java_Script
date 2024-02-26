f508e30c23f2086782559ecb3e7b76996780381b
UASTRONGMAN <shkolniy.maks@ukr.net>
2024-02-26 23:06:39 +0200

    Lesson 3

--- Lesson 2/if.js
+++ Lesson 2/if.js
@@ -114,5 +114,6 @@ if (coursesAndDurationArray[4].monthDuration > 5) {
 }
 if (coursesAndDurationArray[5].monthDuration > 5) {
     console.log('Супер')
+
 }
 
---
+++ Lesson 3/main.js
@@ -0,0 +1,36 @@
+// -------------------------First task-------------------------
+// for (let i = 1; i <= 10; i++) {
+//     document.write('<div>Okten</div>');
+// }
+
+
+// -------------------------Second task-------------------------
+// for (let i = 1; i <= 10; i++) {
+//     document.write('<div>This is block #' + i + '</div>');
+// }
+
+
+// -------------------------Third task-------------------------
+// let i = 1;
+// while (i <= 20) {
+//     document.write('<h1>Okten</h1>');
+//     i++;
+// }
+//
+//
+// -------------------------Fourth task-------------------------
+// let i = 1;
+// while (i <= 20) {
+//     document.write('<h1>This is block #' + i + '</h1>');
+//     i++;
+// }
+
+
+// -------------------------Fifth task-------------------------
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// for (let listOfItem of listOfItems) {
+//     document.write('<ul><li>' + listOfItem + '</li></ul>')
+// }
+
+
+// -------------------------Sixth task-------------------------
