1d563594491d0d12772b07b25e7a22100197805e
UASTRONGMAN <shkolniy.maks@ukr.net>
2024-02-28 23:40:45 +0200

    Lesson 4

---
+++ Lesson 4/main.js
@@ -0,0 +1,57 @@
+// -------------------------First task-------------------------
+// function rectangleSquare (a, b) {
+//     return a * b
+// }
+
+// -------------------------Second task-------------------------
+// function circleSquare (r) {
+//     return Math.PI*r**2
+// }
+
+
+// -------------------------Third task-------------------------
+// function cylinderSquare (h, r) {
+//     return Math.PI*2*h*r
+// }
+
+
+// -------------------------Fourth task-------------------------
+// let arr = [1, 2, 3, 4, 5, 6]
+// function array () {
+//     for (let a of arr) {
+//         console.log(a)
+//     }
+// }
+// array()
+
+
+// -------------------------Fifth task-------------------------
+// function text (p) {
+//     document.write(`<p>${p}</p>`)
+// }
+// text("Lorem ipsum.")
+
+
+// -------------------------Sixth task-------------------------
+// document.write(`<ul>`)
+// function list (li) {
+//     document.write(`<li>${li}</li>`)
+//     document.write(`<li>${li}</li>`)
+//     document.write(`<li>${li}</li>`)
+// }
+// document.write(`</ul>`)
+// list('wedwf')
+
+
+// -------------------------Seventh task-------------------------
+// document.write(`<ul>`)
+// function list (text, n) {
+//     for (let i = 0; i < n; i++) {
+//         document.write(`<li>${text}</li>`);
+//     }
+// }
+// document.write(`</ul>`)
+// list('wedwf', 10);
+
+
+// -------------------------Eighth task-------------------------
